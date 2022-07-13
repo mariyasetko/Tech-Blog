e9314c870ad86d7500beae14a43e0b037dfc25c4
aproblemlike <you@example.com>
2022-07-12 21:42:40 -0400

    set up routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const {
+  User,
+  Posts
+} = require('../models');
+
+// LOGIN route
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
+// SIGNUP route
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
+module.exports = router;
